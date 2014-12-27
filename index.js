b13df3b8b3aca87071f9bb9ff0ac7b717ce4d005
Seth Lakowske <lakowske@gmail.com>
2014-12-27 02:02:29 -0600

    print the headers too

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/pushreq', function(req,res) {
-
+    console.log(req.headers);
     console.log(req.body);
 /*
     //get our current branch

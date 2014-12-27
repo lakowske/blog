bfc65df1d81b2030c5fa70ad0624e6e932468063
Seth Lakowske <lakowske@gmail.com>
2014-12-27 01:24:59 -0600

    test change

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ var app = server.app;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
 app.post('/pushreq', function(req,res) {
 
     console.log(req.body);

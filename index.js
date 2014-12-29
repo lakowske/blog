f360bce1a176d9838d9633c7b0cf63f35495091d
Seth Lakowske <lakowske@gmail.com>
2014-12-29 09:24:06 -0600

    removed unnused dependencies

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
 var gitPull        = require('git-pull');
 var git            = require('git-rev')
 var createHandler  = require('github-webhook-handler');
-var handler        = createHandler({ path: '/webhook', secret: 'myhashsecret' })
-var WsStaticServer = require('websocket-express').WsStaticServer;
-var bodyParser     = require('body-parser');
+var http           = require('http');
 var fs             = require('fs');
 
 //the path(s) we want to serve
@@ -15,22 +13,10 @@ var path = __dirname + '/articles';
 
 console.log(path);
 
-var server = new WsStaticServer({
-    path   : 'path',
-    wsPath : '/webSocket'
-})
-
-var app = server.app;
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 var bodyFile = fs.createWriteStream('pushEvent.txt');
 
-app.post('/pushreq', function(req,res) {
-    console.log(req.headers);
+var server   = http.createServer(function(req, res) {
     req.pipe(bodyFile);
-    //console.log(req.body);
-    res.send('ok');
-
 })
 
 server.listen(3333);

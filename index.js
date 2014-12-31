a8910746aa96ec2eee56ac88e984ce3ae4d03050
Seth Lakowske <lakowske@gmail.com>
2014-12-30 23:44:22 -0700

    added link

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var WsStaticServer = require('websocket-express').WsStaticServer;
 var Deployer       = require('github-webhook-deployer');
 
 //the port we want to serve from
-var appPort = 3333
+var appPort        = 3333
 
 //the path(s) we want to serve
-var path = __dirname + '/articles';
+var path           = __dirname + '/articles';
 
 //let the world know
 console.log(path);

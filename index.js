7bd156aa804f69276c5ec53f90a5f5a65699368c
Seth Lakowske <lakowske@gmail.com>
2015-01-10 23:18:46 -0600

    deployerPort variable added

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ var Deployer       = require('github-webhook-deployer');
 //the port we want to serve from
 var appPort        = parseInt(process.argv[2], 10);
 
+//deployment port listening for github push events
+var deployerPort   = appPort+1;
+
 //the path(s) we want to serve
 var path           = __dirname + '/articles';
 
 //Create a github deployer
 var deployer = new Deployer({path:'/webhook', secret : 'testSecret'});
+
+console.log('listening on port ' + deployerPort);
+
 deployer.listen(appPort+1);
 
 //Create a static server with websockets

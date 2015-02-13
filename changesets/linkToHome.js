385fe453f492d1dba89b0f901c8aea586109dcfe
Seth Lakowske <lakowske@gmail.com>
2015-02-12 22:09:39 -0600

    link to sethlakowske.com and transform script

--- changesets/linkToHome.js
+++ changesets/linkToHome.js
@@ -4,12 +4,49 @@
 
 var articles = require('blog-articles');
 var trumpet  = require('trumpet');
-var fs = require('fs')
+var fs       = require('fs')
+var walk     = require('walk');
+var through  = require('through');
 
-var tr = trumpet();
-tr.pipe(process.stdout);
+function inject() {
+    var articleDir = '../articles';
+    var walker     = walk.walk(articleDir);
 
-var ws = tr.select('body > h3').createWriteStream();
-ws.end('Posted by: <a href="sethlakowske.com">Seth Lakowske</a>');
 
-fs.createReadStream('../articles/github-push-event-deployment/index.html').pipe(tr);
+    walker.on('file', function(root, stat, next) {
+
+        var match = /index\.html/.test(stat.name);
+        if (match) {
+            var articlePath = root + '/' + stat.name;
+            console.log(articlePath);
+
+            transformFile(articlePath, updateLink());
+        }
+
+        next();
+    })
+}
+
+function updateLink() {
+    var tr = trumpet();
+    var ws = tr.select('body > h3').createWriteStream();
+    ws.end('Posted by: <a href="sethlakowske.com">Seth Lakowske</a>');
+
+    return tr;
+}
+
+function transformFile(file, transform) {
+    var rs = fs.createReadStream(file);
+    var rand = Math.random() * 1E8;
+    var tmpFile = Math.round(rand).toString();
+    var ws = fs.createWriteStream(tmpFile);
+    rs.pipe(transform).pipe(ws);
+
+    ws.on('finish', function() {
+        console.log('all done');
+        fs.unlinkSync(file);
+        fs.renameSync(tmpFile, file);
+    })
+}
+
+inject();

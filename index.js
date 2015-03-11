30e0d2f3ce4c3a74eacbbf5d7db688d508763320
Seth Lakowske <lakowske@gmail.com>
2015-03-11 00:05:35 -0500

    started an explanation of the benefits of emacs lisp

--- index.js
+++ index.js
@@ -41,13 +41,40 @@ var server = http.createServer(function(req, res) {
 
 }).listen(argv.port);
 
-console.log(__dirname + '/articles');
+var articleDir = __dirname + '/articles';
+var mount      = '/articles';
 
-articles('/articles', __dirname + '/articles', router, function() {
+//Get a set of discovered articles
+articles(mount, articleDir, function(discovered) {
+
+    //Apply url generation step
+    var urls = discovered.map(function(article) {
+
+        //Generated url to respond to (could be multiple urls if desired)
+        var url = mount + '/' + article;
+
+        //Lamda to apply on url request
+        router.addRoute(url, function(req, res, params) {
+            var articleStream = fs.createReadStream(path.join(articleDir, article, 'index.html'));
+
+            var related = trumpet();
+            var ws      = related.createWriteStream('#related');
+            var stand   = linkstand.toHTML(discovered, urls);
+            stand.pipe(ws);
+
+            res.setHeader('content-type', 'text/html');
+
+            //Compose the article and pipe to response
+            articleStream.pipe(related).pipe(reqstats).pipe(res);
+        })
+
+    })
+
     server.listen(argv.port, function () {
         console.log('listening on :' + server.address().port);
     });
-});
+
+})
 
 //deployment port listening for github push events
 var deployerPort   = argv.port + 1;

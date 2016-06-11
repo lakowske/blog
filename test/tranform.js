1d8056f9bed9ce8d0327d25d1148b11fbded73f2
Seth Lakowske <lakowske@gmail.com>
2016-06-11 14:46:28 -0500

    test/tranform.js

--- test/tranform.js
+++ test/tranform.js
@@ -43,7 +43,7 @@ test('can read html', function(t) {
     })
 
     var html = fs.createReadStream('../articles/howto-install-docker-kubernets-local-registry/index.html');
-    //html.pipe(tr).pipe(process.stdout);
+    html.pipe(tr).pipe(process.stdout);
 
     t.end();
     
@@ -56,7 +56,7 @@ test('can append to tag', function(t) {
     console.log(defaults);
     var tr = append('head', '<link rel="stylesheet" type="text/css" href="/static/style/syntax.css">');
     var html = fs.createReadStream('../articles/howto-install-docker-kubernets-local-registry/index.html');
-    html.pipe(tr).pipe(process.stdout);
+    //html.pipe(tr).pipe(process.stdout);
 
     t.end();
 
